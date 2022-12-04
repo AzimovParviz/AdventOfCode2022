2629cc5620998e18071fc5acbe2825b036b7ee68
Parviz Azimov <parviz.azimov@student.lamk.fi>
2022-12-04 09:33:19 +0200

    day 4 part 2 and change to part 1
    Signed-off-by: Parviz Azimov <parviz.azimov@student.lamk.fi>

--- day4/part2.cpp
+++ day4/part2.cpp
@@ -24,26 +24,33 @@ int getPoints()
 		secondBegin = secondAssignment.substr(0, secondAssignment.find("-"));
 		secondEnd = secondAssignment.substr(secondAssignment.find("-")+1, secondAssignment.length());
 		//if beginning is less than the second's and the end is bigger than the second's
-		std::cout<<firstAssignment<<" "<<secondAssignment<<'\n';
+//		std::cout<<firstAssignment<<" "<<secondAssignment<<'\n';
 //		std::cout<<firstBegin<<' '<<secondBegin<<'\n';
 //		std::cout<<firstEnd<<' '<<secondEnd<<'\n';
-	
+		/*
+		 * 2-4 6-8
+		 * 2-3 4-5
+		 */
 		if(
 		  ((std::stoi(firstBegin) <= std::stoi(secondBegin))
-		  && (std::stoi(firstEnd) >= std::stoi(secondEnd))) 
-		  
+		  && (std::stoi(firstEnd) >= std::stoi(secondEnd)))  
 		  ||
 		  (
 		   (std::stoi(firstBegin) >= std::stoi(secondBegin))
-		   &&
-		   (std::stoi(firstEnd) <= std::stoi(secondEnd))
+		   && (std::stoi(firstEnd) <= std::stoi(secondEnd))
 		  )
+
+		  ||
+
+		  (
+		    (std::stoi(secondEnd) >= std::stoi(firstBegin))
+		   && ((std::stoi(firstEnd) >= std::stoi(secondBegin)))
+		   )
 		  )
 		{
-			std::cout<<secondAssignment<<" is fully contained by "<<firstAssignment<<'\n';
 			containedCount++;
+			std::cout<<firstAssignment<<" "<<secondAssignment<<'\n';
 		}
-
 	}
 
 	return containedCount;
